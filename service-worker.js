75780ae7627fcccee085c4e307342b4c6063ac0a
estudiantestudio <estudiantestudio278@gmail.com>
2026-07-17 23:57:08 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE = "focus-cero-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseCopy = networkResponse.clone();
+
+          caches.open(CACHE).then((cache) => {
+            cache.put(event.request, responseCopy);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
